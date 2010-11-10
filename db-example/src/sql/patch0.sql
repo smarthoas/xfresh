159ce5d51e5d50710fd485026814b0b4f8c8cb51
Olga Bolshakova <olgab-87@yandex.ru>
2010-11-11 01:59:37 +0300

    1) db example log file updated (db-example.log) 2) database creation added to patch0.sql (db-example)

--- db-example/src/sql/patch0.sql
+++ db-example/src/sql/patch0.sql
@@ -1,11 +1,14 @@
-/*структура для хранения проверок*/
+--создание базы данных
+CREATE DATABASE test;
+
+--структура для хранения проверок
 DROP TABLE IF EXISTS db_check;
 CREATE TABLE db_check (
   query    TEXT NOT NULL,
   dsc    TEXT NOT NULL
 ) CHARACTER SET utf8;
 
-/*пример проверок*/
+--пример проверок
 INSERT INTO db_check (query, dsc) VALUES
   ('select 1 result, \'ERROR\' dsc', 'Error example'),
   ('select 0 result, \'OK\' dsc', 'OK example');
